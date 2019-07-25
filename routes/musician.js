65c3e750a668a689e52985b9fb2f23149b9f7de2
Pushparaj <pushparaj@wellteq.co>
2019-07-25 17:49:54 +0800

    Update musician.js

--- routes/musician.js
+++ routes/musician.js
@@ -6,7 +6,7 @@ const schema = require('../store/schema');
 
 // healthcheck
 router.get('/health', (req, res) => {
-  res.status('200').send("Status: ok!");
+  res.status('200').send("I am healthy!");
 });
 
 // retrieve all musicians from data store
